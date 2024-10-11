8db6b6b53b62effee5cba4a143bde79c87a666ac
Simon-0000 <asmarsimon@hotmail.com>
2024-10-11 18:29:09 -0400

    cleaned up test code mb

--- main/main.cpp
+++ main/main.cpp
@@ -12,12 +12,10 @@ extern "C" void app_main(void)
     ESP_LOGI(TAG_MAIN,"HELLOOO!");
     MCPWM<MCPWM_TIMER_CLK_SRC_DEFAULT,McpwmConfigs::TimerGroup::GROUP_0> mcPWM(0,mcpwm_timer_count_mode_t::MCPWM_TIMER_COUNT_MODE_UP,1000000,20000);
     Gpio directionPin(gpio_num_t::GPIO_NUM_8,gpio_mode_t::GPIO_MODE_OUTPUT);
-    // directionPin.setState(true);
     DCMotor motor(&mcPWM,&directionPin,true);
-    // mcPWM.setPwmTicks(19000);
+
     while(true){
-        for(int i = INT32_MIN; i < INT32_MAX; i+=2000000){
-            directionPin.setState(i%2);
+        for(int i = INT32_MIN; i < INT32_MAX; i+= (UINT32_MAX / 255)){
             ESP_LOGI(TAG_MAIN,"speed: %d, pwm(percentage): %f direction: %d",i,abs(i)/(float)INT32_MAX * 100,i > 0);
             motor.setSpeed(static_cast<int32_t>(i));
             vTaskDelay(10/portTICK_PERIOD_MS);
